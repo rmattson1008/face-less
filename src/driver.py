7afa2bc3ba966a17dd87ff6ecc41434499c51be7
rmattson1008 <rachelmattson1008@gmail.com>
2022-10-27 15:09:43 -0400

    get one working pass through model

--- src/driver.py
+++ src/driver.py
@@ -1,16 +1,32 @@
 import torch
 import torchvision
 import torchvision.datasets as datasets
-from models import VGG_Model
-
+from models import VGG16
+from torchvision import transforms
+from PIL import Image
 
 
 
 if __name__ == "__main__":
-    mnist_trainset = datasets.MNIST(root='./data', train=True, download=True, transform=None)
-    mnist_testset = datasets.MNIST(root='./data', train=False, download=True, transform=None)
-    print(len("Len data to train on"), mnist_trainset)
+    # mnist_trainset = datasets.MNIST(root='./data', train=True, download=True, transform=None)
+    # mnist_testset = datasets.MNIST(root='./data', train=False, download=True, transform=None)
+    # print(len("Len data to train on"), mnist_trainset)
 
-    model = VGG_Model()
+    model = VGG16()
     print(model)
+
+    image = Image.open('samples/AJ_Cook.jpg')
+    t = transforms.Compose([transforms.Resize(224),  transforms.ToTensor()])
+    tensor_image = t(image)
+    print(tensor_image.shape)
+    inp = tensor_image.unsqueeze(dim=0)
+    print(inp.shape)
+
+    out = model(inp)
+    print("Out")
+    print(out)
+    print(out.shape)
+
+
+    
 
--- src/models.py
+++ src/models.py
@@ -3,37 +3,94 @@ import torch
 import torch.nn as nn
 
 
-class VGG_Model(nn.Module):
-    
-    def __init__(self):
-        super().__init__()
-
-
-        self.cnn_layers = nn.Sequential(
-            # BLock 1
-            nn.Conv2d(3,64,3,padding=1), 
-            nn.LeakyReLU(inplace=True),
-            nn.Conv2d(64,128,3,padding=1), 
-            nn.LeakyReLU(inplace=True),
-            nn.MaxPool2d(2, stride=2),
-            # Block 2
-            nn.Conv2d(128,256,3,padding=1), 
-            nn.LeakyReLU(inplace=True),
-            nn.Conv2d(256,256,3,padding=1), 
-            nn.LeakyReLU(inplace=True),
-            nn.MaxPool2d(2, stride=2),
-        )
-
-        # should be length of unwound channels * feature map dims
-        db_size = 256 * 7 * 7
-        # one layer to classify
-        self.linear_layers = nn.Sequential(nn.Linear(db_size, 1024), nn.ELU(), nn.Linear(1024, 512), nn.ELU(), nn.Linear(512, 10), nn.ELU())
-        self.fc = nn.Linear(10,3)
-
-
+class VGG16(nn.Module):
+    def __init__(self, num_classes=10):
+        super(VGG16, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU())
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(), 
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU())
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU())
+        self.layer6 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU())
+        self.layer7 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer8 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU())
+        self.layer9 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU())
+        self.layer10 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer11 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU())
+        self.layer12 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU())
+        self.layer13 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.fc = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(7*7*512, 4096),
+            nn.LeakyReLU())
+        self.fc1 = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.LeakyReLU())
+        self.fc2= nn.Sequential(
+            nn.Linear(4096, num_classes))
+        
     def forward(self, x):
-        x = self.cnn_layers(x)
-        x = x.view(x.size(0), -1)
-        x = self.linear_layers(x)
-        return x
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        out = self.layer6(out)
+        out = self.layer7(out)
+        out = self.layer8(out)
+        out = self.layer9(out)
+        out = self.layer10(out)
+        out = self.layer11(out)
+        out = self.layer12(out)
+        out = self.layer13(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        return out
 
