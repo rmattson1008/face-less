86e6ed30512cb6b5f7d65b278d64f6876a9609bf
rmattson1008 <rachelmattson1008@gmail.com>
2022-11-14 11:01:42 -0500

    improve cropping and add horizontal flip

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,30 +1,33 @@
 
 import torchvision
 import torch
+from torchvision import transforms as T
+from torchvision.transforms.functional import InterpolationMode
 
 class OccTransform:
     """sdfgasfga"""
 
-    def __init__(self, width=112, height=112):
+    def __init__(self, mask_size=224, random_horizontal_flip=True):
         """
             width and hieght extend from (0,0) which is the left corner of an image
         """
-        self.crop_width = width
-        self.crop_height = height
+        self.mask_size= mask_size
+        self.flip = random_horizontal_flip
 
     def __call__(self, sample):
         """
         sample is [n_channels, img_width, img_height]
         """
-        print(sample.shape)
-        # you will hate the way I constructed this mask
-        mask = torch.zeros((112,112))
-        pad = torch.ones((112,112))
-        mask = torch.cat((mask, pad))
-        pad = torch.cat((pad, pad))
-        mask = torch.cat((mask, pad), axis=1)
+        # print(sample.shape)
 
-        assert mask.shape == sample[0].shape
+        mask = torch.tensor([[0,1], [1,1]]).unsqueeze(0)
+        mask = T.Resize(self.mask_size, interpolation=InterpolationMode.NEAREST)(mask)
+        if self.flip:
+            mask = T.RandomHorizontalFlip(p=0.5)(mask)
+
+        # print(mask.shape)
+        # print(sample.shape)
+        assert mask[0].shape == sample[0].shape
         cropped = mask * sample
-
+        # print(cropped.shape)
         return cropped
