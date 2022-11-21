549d345d1cb0da4802a584d9d5f22cc15208ac30
Nicholas Akin <49415242+nikakcin17@users.noreply.github.com>
2022-11-21 14:21:35 -0500

    Added normalization to image tensors

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,33 +1,37 @@
-
 import torchvision
 import torch
 from torchvision import transforms as T
 from torchvision.transforms.functional import InterpolationMode
 
 class OccTransform:
-    """sdfgasfga"""
 
-    def __init__(self, mask_size=224, random_horizontal_flip=True):
+    def __init__(self, transform, mask_size=224): 
         """
             width and hieght extend from (0,0) which is the left corner of an image
+            Default mask upper-left occlusion
         """
-        self.mask_size= mask_size
-        self.flip = random_horizontal_flip
+        self.mask_size = mask_size
+        self.transform = transform 
 
     def __call__(self, sample):
         """
         sample is [n_channels, img_width, img_height]
         """
-        # print(sample.shape)
+        print(sample.shape)
+        print(sample)
 
-        mask = torch.tensor([[0,1], [1,1]]).unsqueeze(0)
+        #upper-left mask 
+        mask = torch.tensor([[0,1], [1,1]]).unsqueeze(0)    #(224, 224, 3) to (1, 3, 224, 224) 
+
+        #applying custom transform
+        if self.transform:
+            mask = torch.tensor([[0,1], [0,1]]).unsqueeze(0)    
+        
         mask = T.Resize(self.mask_size, interpolation=InterpolationMode.NEAREST)(mask)
-        if self.flip:
-            mask = T.RandomHorizontalFlip(p=0.5)(mask)
 
-        # print(mask.shape)
-        # print(sample.shape)
         assert mask[0].shape == sample[0].shape
         cropped = mask * sample
         # print(cropped.shape)
         return cropped
+
+
--- src/driver.py
+++ src/driver.py
@@ -2,12 +2,13 @@ import torch
 import torchvision
 import torch.nn as nn
 import torchvision.datasets as datasets
+from torchvision import os
 from models import VGG16 #custom vgg16 model
 from vgg19 import VGG19
-from torchvision import os
+from torchvision import transforms
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
-from torch.utils.tensorboard import SummaryWriterm
+from torch.utils.tensorboard import SummaryWriter
 from torch.optim import Adam
 from torch.nn import CrossEntropyLoss
 import argparse
@@ -17,8 +18,13 @@ import torchvision.transforms.functional as TF
 import numpy as np
 import random
 
-# Set random seed
-# shorturl.at/bKO38
+
+
+"""
+Setting seed for uses of random
+
+    # shorturl.at/bKO38
+"""
 def set_seed(seed: int = 42) -> None:
     np.random.seed(seed)
     random.seed(seed)
@@ -30,7 +36,6 @@ def set_seed(seed: int = 42) -> None:
     # Set a fixed value for the hash seed
     os.environ["PYTHONHASHSEED"] = str(seed)
     print(f"Random seed set as {seed}")
-
 
 
 """
@@ -102,7 +107,7 @@ def train(args, model, train_loader, val_loader):
         num_batches_used = 0.0 
 
         print("Epoch", epoch)
-        for batch_idx, (images, labels) in enumerate(train_loader):
+        for batch_idx, (images, labels) in enumerate(tqdm(train_loader)):
             optimizer.zero_grad()
             images = images.to(args.device)
             labels = labels.to(args.device)
@@ -126,25 +131,23 @@ def train(args, model, train_loader, val_loader):
         tb.add_scalar("TrainLoss", train_loss, epoch)
         tb.add_scalar("TrainAccuracy", train_acc, epoch)
 
-        # for batch_idx, (inputs, labels) in enumerate(val_loader):
-        #     inputs = inputs.to(args.device)
-        #     labels = labels.to(args.device)
-        #     print("-----INPUTS-----")
-        #     print(inputs)
-        #     preds, _ = model(inputs)
-        #     loss = loss_fn(preds, labels)
+        for batch_idx, (inputs, labels) in enumerate(val_loader):
+            inputs = inputs.to(args.device)
+            labels = labels.to(args.device)
+            preds, _ = model(inputs)
+            loss = loss_fn(preds, labels)
 
-        #     val_loss += loss.item()
-        #     other_val_loss += loss.item() 
-        #     val_total_correct += preds.argmax(dim=1).eq(labels).sum().item()
-        #     real_val_total_correct = preds.argmax(dim=1).eq(labels).sum().item()
-        #     val_acc += real_val_total_correct / len(labels)
-        #     num_batches_used = batch_idx + 1
+            val_loss += loss.item()
+            other_val_loss += loss.item() 
+            val_total_correct += preds.argmax(dim=1).eq(labels).sum().item()
+            real_val_total_correct = preds.argmax(dim=1).eq(labels).sum().item()
+            val_acc += real_val_total_correct / len(labels)
+            num_batches_used = batch_idx + 1
 
-        # val_loss = val_loss / num_batches_used
-        # val_acc = val_acc / num_batches_used * 100
-        # tb.add_scalar("ValLoss", val_loss, epoch)
-        # tb.add_scalar("ValAccuracy", val_acc, epoch)
+        val_loss = val_loss / num_batches_used
+        val_acc = val_acc / num_batches_used * 100
+        tb.add_scalar("ValLoss", val_loss, epoch)
+        tb.add_scalar("ValAccuracy", val_acc, epoch)
 
 
 
@@ -161,7 +164,7 @@ def test(args, model, test_loader):
     with torch.no_grad():
         correct = 0
         total = 0
-        for i, (images, labels) in tqdm(test_loader):
+        for (images, labels) in tqdm(test_loader):
             images = images.to(args.device)
             labels = labels.to(args.device)
             outputs = model(images)
@@ -171,6 +174,34 @@ def test(args, model, test_loader):
     print('Accuracy of the network on the {} test images: {} %'.format(10000, 100 * correct / total))  
     return
 
+"""
+helper function for mean and sd for data normalization
+"""
+def batch_mean_and_sd(loader):
+    
+    cnt = 0
+    fst_moment = torch.empty(3)
+    snd_moment = torch.empty(3)
+
+    for images, _ in loader:
+        b, c, h, w = images.shape
+        nb_pixels = b * h * w
+        sum_ = torch.sum(images, dim=[0, 2, 3])
+        sum_of_square = torch.sum(images ** 2,
+                                  dim=[0, 2, 3])
+        fst_moment = (cnt * fst_moment + sum_) / (
+                      cnt + nb_pixels)
+        snd_moment = (cnt * snd_moment + sum_of_square) / (
+                            cnt + nb_pixels)
+        cnt += nb_pixels
+
+    mean, std = fst_moment, torch.sqrt(
+      snd_moment - fst_moment ** 2)        
+    return mean,std
+  
+# mean, std = batch_mean_and_sd(loader)
+# print("mean and std: \n", mean, std)
+
 
 
 """
@@ -178,9 +209,6 @@ Main Function
 
 """
 if __name__ == "__main__":
-    # set random seed
-    set_seed()
-
     # mnist_trainset = datasets.MNIST(root='./data', train=True, download=True, transform=None)
     # mnist_testset = datasets.MNIST(root='./data', train=False, download=True, transform=None)
 
@@ -202,22 +230,27 @@ if __name__ == "__main__":
 
     #download lfw deep funneled dataset if not downloaded
     # lfw_train = datasets.LFWPeople(root: './data', split: str = '10fold', image_set: str = 'funneled', transform: Optional[Callable] = None, target_transform: Optional[Callable] = None, download: bool = False)
-    t = transforms.Compose([transforms.Resize(args.resize),  transforms.ToTensor(), OccTransform(mask_size=args.resize)])
+
+
+    t = transforms.Compose([transforms.Resize(args.resize),  
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]), #optional Normalization
+        OccTransform(True, mask_size=args.resize)]) #optional: boolean is for additional mask 
+
     lfw_train = datasets.LFWPeople(root= './data', split= 'Train', image_set='deepfunneled', transform=t,  download= True)
     lfw_test = datasets.LFWPeople(root= './data', split= 'Test', image_set='deepfunneled', transform=t , download=True)
 
 
+    lfw_train, lfw_val = torch.utils.data.random_split(lfw_train, [.8, .2], generator=torch.Generator().manual_seed(42))
+
     print("lfw_train: ", lfw_train)
     print("type lfw_train: ", type(lfw_train))
-
-    lfw_train,  lfw_val = torch.utils.data.random_split(lfw_train, [.8, .2], generator=torch.Generator().manual_seed(42))
     print("lfw_train Length: ", lfw_train)
-    lfw_train.transform = transforms.Compose([transforms.Resize(args.resize),  transforms.ToTensor(), OccTransform(mask_size=args.resize)])
-    
+
+
     train_loader = DataLoader(lfw_train, batch_size=16, shuffle=False)
     val_loader = DataLoader(lfw_val, batch_size=16, shuffle=False)
     test_loader = DataLoader(lfw_test, batch_size=16, shuffle=False)
-
 
 
 
@@ -230,61 +263,29 @@ if __name__ == "__main__":
     "VGG19": [ 64, 64, "M", 128, 128, "M", 256, 256, 256, 256, "M", 512, 512, 512, 512, "M", 512, 512, 512, 512, "M", ],
     }
 
-    #create CNN based on vgg19 architecture
-    
+
     print("========== Build Model==========")
     model = VGG19(in_channels=3, in_height=224, in_width=224, num_classes=5749, architecture=VGG_types["VGG19"])
-
-   # model = VGG19(VGG_types["VGG19"])
+    model.to(args.device)
     print("model: ", model)
 
 
 
-    #if gpu is available, run on cuda enabled gpu
-    if torch.cuda.is_available(): 
-        model.cuda()
-
     print("========== Model Architecture ==========")
     print(model) #show model architecture
+
 
     print("========== Model Summary ==========")
     print(model, (1,3,224,224)) #model summary
 
 
-
-    # inp, y = next(iter(lfw_train))
-    # inp = inp.unsqueeze(dim=0)
-    # print(inp.shape)
-    # print('y', y)
-
-
-
-
-
-    # image = Image.open("./samples/AJ_Cook/AJ_Cook.jpg")
-    # image.show()
-    # image = image.resize((224,224))
-
-    # x = TF.to_tensor(image) #converts image into tensor array
-    # #converts image (height, width, channel) into (batch_size, height, width, channel) 
-    # x.unsqueeze_(0)            #(224, 224, 3) to (1, 3, 224, 224) 
-    # x=x.to("cuda")             #convert to cuda
-    # print(x.shape)             #testing shape 
-
-
-    # pred = model(x)
-    # pred_numpy = pred.detach().numpy()
-    # pred_class = np.argmax(pred_numpy)
-    # pred_class
-
-
-    # torch.backends.cudnn.enabled==False #resolves - cuDNN error: CUDNN_STATUS_MAPPING_ERROR
-    #RuntimeError: cuDNN error: CUDNN_STATUS_EXECUTION_FAILED
     print("========== Train Model==========")
-    #train(args, model, train_loader, val_loader)
+    train(args, model, train_loader, val_loader)
 
     print("========== Test Model==========")
-    #test(args, model, test_loader)
+    # test(args, model, test_loader)
 
     # torch.save(model.state_dict(),  'model_weights.pth')
+
+
 
